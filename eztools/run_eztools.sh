0e04058c9f915d6d922215f4b3586c32f347dbb7
mf1d3l <hrfor@FORENSIC.>
2025-06-19 19:34:02 +0200

    run_eztools upd

--- eztools/run_eztools.sh
+++ eztools/run_eztools.sh
@@ -24,4 +24,18 @@ INPUT_PATH="$1"
 OUTPUT_PATH="$2"
 
 find "$INPUT_PATH" -type f -name '$MFT' -exec $mftecmd -f {} --csv "$OUTPUT_PATH" \;
+find "$INPUT_PATH" -type f -name '$MFT' -exec $mftecmd -f {} --body "$OUTPUT_PATH" --blf --bdl C: \;
+find "$OUTPUT_PATH" -type f -name '*.body' -exec mactime -y -d -b {} > "$OUTPUT_PATH"/timeline_"$(date +%s)".csv \;
+find "$OUTPUT_PATH" -type f -name '*.body'-exec rm {} \;
+find "$INPUT_PATH" -type f -name '$J' -exec $mftecmd -f {} --csv "$OUTPUT_PATH" \;
+$recmd --bn /opt/RECmd/RECmd/BatchExamples/Kroll_Batch.reb -d "$INPUT_PATH" --csv "$OUTPUT_PATH"
+find "$INPUT_PATH" -type f -wholename '*/Windows/System32/config/SYSTEM' -exec $appcompatcacheparser -f {} --csv "$OUTPUT_PATH" \;
+find "$INPUT_PATH" -type f -name 'Amcache.hve' -exec $amcacheparser -f {} --csv "$OUTPUT_PATH" \;
+find "$INPUT_PATH" -type d -wholename '*/winevt/logs' -exec $evtxecmd -d {} --csv "$OUTPUT_PATH" \;
 
+for USERPATH in $(find "$INPUT_PATH" -type f -name UsrClass.dat | grep -oP '.*/Users/[^/]+' ) ;do
+  USER=$(echo $USERPATH | grep -oP "[^/]+$");
+  $sbecmd -d $USERPATH --csv "$OUTPUT_PATH" --csvf SBECmd_$USER.csv
+  find "$USERPATH" -type d -wholename '*/Microsoft/Windows/Recent' -exec $lecmd -d {} --csv "$OUTPUT_PATH" --csvf LECmd_$USER.csv \;
+  find "$USERPATH" -type d -wholename '*/Microsoft/Windows/Recent/AutomaticDestinations' -exec $jlecmd -d {} --csv "$OUTPUT_PATH" --csvf JLECmd_$USER.csv \;
+done
